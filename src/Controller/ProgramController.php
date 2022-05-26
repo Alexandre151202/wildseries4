eab65534f7adacf3adc9e629590050a9dfa72210
Alexandre Saint-Martin <alexandresaintmartin33@gmail.com>
2022-05-26 16:13:29 +0200

    wip

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,23 +1,29 @@
 <?php
-
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route('/', name: 'program')]
-Class ProgramController extends AbstractController
+
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'index')]
+    #[Route('/program/', name: 'index')]
     public function index(): Response
     {
-//
-            $wildSeries = '<!doctype html><html lang="en"><title>Wild Series</title><body>Wild Series Index</body></html>';
-//
-//        );
-        return $this->render('index.html.twig', [
-            'website' => $wildSeries,
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
         ]);
     }
+
+    #[Route('/{id<\d+>}', name: 'view', methods: ['GET'])]
+    public function show($id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+
 }
