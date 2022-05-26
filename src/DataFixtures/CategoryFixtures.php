bd984f34e3ce3fdb44a31e20e589de4f15e56fbb
Alexandre Saint-Martin <alexandresaintmartin33@gmail.com>
2022-05-26 16:39:35 +0200

    wip

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+
+        for ($i = 1; $i <= 50; $i++) {
+            $category = new Category();
+            $category->setName('Nom de catégorie ' . $i);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+    const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur',
+    ];
+
+}
